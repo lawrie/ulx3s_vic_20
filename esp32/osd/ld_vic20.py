fae5366b3e154f5b7491cd2cc0a5bd000a701195
emard <vordah@gmail.com>
2020-05-07 08:28:14 +0200

    file reading, reset seems working

--- esp32/osd/ld_vic20.py
+++ esp32/osd/ld_vic20.py
@@ -87,8 +87,125 @@ class ld_vic20:
     self.cs.off()
     self.cs.on()
 
+  def read_maincpu(self,s,size):
+    print("READING MAIN CPU STATE")
+    header=bytearray(47)
+    s.readinto(header)
+    bytes_read=len(header)
+    A=header[4]
+    X=header[5]
+    Y=header[6]
+    S=header[7]
+    PC=unpack("<H",header[8:10])[0]
+    P=header[10]
+    print("A=0x%02X X=0x%02X Y=0x%02X S=0x%02X PC=0x%04X P=0x%02X" % (A,X,Y,S,PC,P))
+    print("size %d unknown bytes to read %d" % (size, size-bytes_read))
+    s.seek(size-bytes_read,1)
+    return True
+
+  def read_vic20mem(self,s,size):
+    print("READING RAM")
+    header=bytearray(4)
+    s.readinto(header)
+    bytes_read=len(header)
+    config=header[0]
+    print("config",config)
+    if True:
+      s.seek(1*1024,1) # 0017-0416 - 1K RAM dump ($0000-03FF)
+      s.seek(4*1024,1) # 0417-1416 - 4K RAM dump ($1000-1FFF)
+      bytes_read+=5*1024
+    if config&1: # 1B17-2816 - 3K RAM dump ($0400-0FFF, if CONFIG bit 0 set)
+      print("3K RAM block present at $0400-0FFF")
+      s.seek(3*1024,1)
+      bytes_read+=3*1024
+    if config&2: # 2817-4816 - 8K RAM dump ($2000-3FFF, if CONFIG bit 1 set)
+      print("8K RAM block present at $2000-3FFF")
+      s.seek(8*1024,1)
+      bytes_read+=8*1024
+    if config&4: # 4817-6816 - 8K RAM dump ($4000-5FFF, if CONFIG bit 2 set)
+      print("8K RAM block present at $4000-5FFF")
+      s.seek(8*1024,1)
+      bytes_read+=8*1024
+    if config&8: # 6817-8816 - 8K RAM dump ($6000-7FFF, if CONFIG bit 3 set)
+      print("8K RAM block present at $6000-7FFF")
+      s.seek(8*1024,1)
+      bytes_read+=8*1024
+    if config&16: # 8817-A816 - 8K RAM dump ($A000-BFFF, if CONFIG bit 5 set)
+      print("8K RAM block present at $A000-BFFF")
+      s.seek(8*1024,1)
+      bytes_read+=8*1024
+    print("size %d unknown bytes to read %d" % (size, size-bytes_read))
+    s.seek(size-bytes_read,1)
+    return True
+
+  def read_vic1(self,s,size):
+    print("READING COLOR RAM")
+    header=bytearray(78)
+    s.readinto(header)
+    bytes_read=len(header)
+    print("size %d unknown bytes to read %d" % (size, size-bytes_read))
+    s.seek(size-bytes_read,1)
+    return True
+  
+  def read_vice_module(self,s):
+    header=bytearray(0x16)
+    if s.readinto(header):
+      print("vice module header", header)
+      type=header[0:0x10]
+      size=unpack("<I",header[0x12:0x16])[0]-0x16
+      print("size", size)
+      if type[0:9]==bytearray("VIC20MEM\0".encode()):
+        return self.read_vic20mem(s,size)
+      if type[0:6]==bytearray("VIC-I\0".encode()):
+        return self.read_vic1(s,size)
+      if type[0:8]==bytearray("MAINCPU\0".encode()):
+        return self.read_maincpu(s,size)
+      s.seek(size,1)
+      return True
+    else:
+      return False
+    #print(cpu)
+
+
+
   def loadvsf(self,filename):
-    z=open(filename,"rb")
+    f=open(filename,"rb")
+    header=bytearray(0x25+0x15)
+    f.readinto(header)
+    machine=header[0x15:0x25]
+    expect=bytearray("VIC20\0")
+    if machine[0:6]==expect:
+      self.cpu_halt()
+      while self.read_vice_module(f):
+        pass
+      self.code_addr=0xE000
+      self.vector_addr=0xFFFC
+      self.store_rom(32)
+      #self.patch_rom(regs)
+      self.ctrl(3) # reset and halt
+      self.ctrl(1) # only reset
+      self.cpu_continue()
+      # restore original ROM after image starts
+      self.cpu_halt()
+      self.restore_rom()
+      self.cpu_continue() # release reset
+    else:
+      print("unrecognized header")
+      print("header:", header)
+      print("expected:", expect)
+
+  def old_parse(self):
+    f=open("scramble.vsf","rb")
+    header=bytearray(0x25+0x15)
+    f.readinto(header)
+    machine=header[0x15:0x25]
+    print(machine)
+    #print(header)
+    while self.read_vice_module(f):
+      pass
+      
+
+  def old_xyz(self):
     expect=bytearray("ORAO_0AXYPS_PC_ADDR_LEN_DATA\0")
     header=bytearray(len(expect))
     z.readinto(header)
